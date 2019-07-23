9b40c729cafb3c1f8433afd1c6bd4e9b670bfa56
António P. P. Almeida <antonio.almeida@up42.com>
2019-07-23 12:24:39 +0200

    * Removed Built With note on the footer.

--- source/conf.py
+++ source/conf.py
@@ -94,6 +94,9 @@ html_theme_options = {
     "collapse_navigation": False,
 }
 
+# Do not show the built with Sphinx on the footer.
+html_show_sphinx = False
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
