66ef20afc4cb4d1ab52196517106263578f03740
Christoph Rieke <christoph.rieke@up42.com>
2019-12-20 17:47:17 +0100

    Disable clickable zoom (#151)
    * disable clickable zoom for scaled images
    
    * format

--- source/conf.py
+++ source/conf.py
@@ -130,6 +130,9 @@ html_show_sourcelink = False
 # Do not show the copyright.
 html_show_copyright = False
 
+# Disables clickable zoom for scaled images.
+html_scaled_image_link = False
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
