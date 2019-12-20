c37950b75b5353a84cb61ea62d8b2afcd2fa63b8
Christoph Rieke <christoph.rieke@up42.com>
2019-12-20 17:46:22 +0100

    format

--- source/conf.py
+++ source/conf.py
@@ -133,7 +133,6 @@ html_show_copyright = False
 # Disables clickable zoom for scaled images.
 html_scaled_image_link = False
 
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
